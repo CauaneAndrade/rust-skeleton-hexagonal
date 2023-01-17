1a4f5e279496eabac3dd7ff16a5b625d25d33833
Cauane Andrade <cauane.emanuela@hotmail.com>
2023-01-17 00:17:49 -0300

    feat: Add cargo.toml file creation and content, improve overall code structure

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,119 @@
 use std::env;
 use std::fs;
+use std::io::{self, Write};
 use std::path::Path;
 
 fn main() {
-    // Ask for project name
-    println!("Please enter a name for your project:");
+    // Prompt user for project name
     let mut project_name = String::new();
-    std::io::stdin().read_line(&mut project_name).unwrap();
+    print!("Enter project name: ");
+    io::stdout().flush().unwrap();
+    io::stdin().read_line(&mut project_name).unwrap();
     let project_name = project_name.trim();
-
     // Ask for project details
     println!("Please enter a brief description of your project:");
     let mut project_description = String::new();
-    std::io::stdin().read_line(&mut project_description).unwrap();
+    std::io::stdin()
+        .read_line(&mut project_description)
+        .unwrap();
     let project_description = project_description.trim();
-
-    println!("Please enter the path where you want to create the project (leave blank for current directory):");
-    let mut project_path = String::new();
-    std::io::stdin().read_line(&mut project_path).unwrap();
-    let project_path = project_path.trim();
-
-    let project_path = if project_path.is_empty() {
-        env::current_dir().unwrap()
+    // Prompt user for location to create project
+    print!("Enter location to create project: ");
+    io::stdout().flush().unwrap();
+    let mut project_location = String::new();
+    io::stdin().read_line(&mut project_location).unwrap();
+    let project_location = project_location.trim();
+    // If user did not provide location, create project in current directory
+    let current_dir = env::current_dir().unwrap();
+    let project_path = if project_location.is_empty() {
+        current_dir
     } else {
-        Path::new(&project_path).to_path_buf()
+        Path::new(&project_location).to_path_buf()
     };
 
     // Create the project directory
     fs::create_dir_all(project_path.join(project_name)).unwrap();
-
     // Create the hexagonal architecture directories and files
     fs::create_dir_all(project_path.join(project_name).join("src")).unwrap();
-    fs::create_dir_all(project_path.join(project_name).join("src").join("application")).unwrap();
+    fs::create_dir_all(
+        project_path
+            .join(project_name)
+            .join("src")
+            .join("application"),
+    )
+    .unwrap();
     fs::create_dir_all(project_path.join(project_name).join("src").join("domain")).unwrap();
-    fs::create_dir_all(project_path.join(project_name).join("src").join("infrastructure")).unwrap();
+    fs::create_dir_all(
+        project_path
+            .join(project_name)
+            .join("src")
+            .join("infrastructure"),
+    )
+    .unwrap();
+    // Create files in the application folder
+    let application_file = project_path
+        .join(project_name)
+        .join("src")
+        .join("application")
+        .join("mod.rs");
+    fs::File::create(application_file).unwrap();
+
+    // Create files in the domain folder
+    let domain_file = project_path
+        .join(project_name)
+        .join("src")
+        .join("domain")
+        .join("mod.rs");
+    fs::File::create(domain_file).unwrap();
+
+    // Create files in the infrastructure folder
+    let infrastructure_file = project_path
+        .join(project_name)
+        .join("src")
+        .join("infrastructure")
+        .join("mod.rs");
+    fs::File::create(infrastructure_file).unwrap();
+
+    // Create files in the application folder
+    let application_file = project_path
+        .join(project_name)
+        .join("src")
+        .join("application")
+        .join("mod.rs");
+    fs::File::create(application_file).unwrap();
 
     // Create main.rs file
     let main_file = project_path.join(project_name).join("src").join("main.rs");
     fs::File::create(main_file).unwrap();
 
-    println!("Successfully created new project at {:?} with name {} and description: {}", project_path, project_name, project_description);
+    // Create cargo.toml file
+    let cargo_toml_path = project_path.join(project_name).join("Cargo.toml");
+    fs::File::create(&cargo_toml_path).unwrap();
+
+    // Write to cargo.toml file
+    let mut cargo_toml = fs::OpenOptions::new()
+        .write(true)
+        .open(&cargo_toml_path)
+        .unwrap();
+    cargo_toml
+        .write_all(
+            format!(
+                r#"[package]
+name = "{}"
+version = "0.1.0"
+authors = ["Your Name"]
+description = "{}"
+
+[dependencies]
+"#,
+                project_name, project_description
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+
+    println!(
+        "Project {} successfully created at {:?}",
+        project_name, project_path
+    );
 }
