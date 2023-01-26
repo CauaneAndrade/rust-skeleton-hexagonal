e6830b5d3da579c835dae8a880e701cd8d3c24ab
Cauane Andrade <cauane.emanuela@hotmail.com>
2023-01-25 23:42:50 -0300

    feat: Add unit tests for file manager module

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use crate::args::AppArguments;
 use crate::file_manager::{DiskEntry, DiskEntryType, FileManager};
 use lazy_static::lazy_static;
-use std::path::{PathBuf};
+use std::path::PathBuf;
 
 use crate::result::Result;
 
@@ -77,20 +77,28 @@ impl App {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    use tempfile::tempdir;
     use crate::args::AppArguments;
+    use std::fs;
+    use tempfile::tempdir;
+
+    fn setup() -> (App, PathBuf) {
+        let temp_dir = tempdir().unwrap();
+        let temp_path = temp_dir.path().to_path_buf();
+        let args = AppArguments::new(
+            "test_project",
+            "test details",
+            Some(temp_path.to_str().unwrap().to_owned()),
+        );
+        let app = App::new(&args);
+        (app, temp_path)
+    }
 
     #[test]
     fn test_app_run_success() {
-        let temp_dir = tempdir().unwrap();
-        let temp_path = temp_dir.path().to_path_buf();
-        // let temp_path = temp_dir.path().join("test_project").to_path_buf();
-        let args = AppArguments::new("test_project", "test details", Some(temp_path.to_str().unwrap().to_owned()));
-        let app = App::new(&args);
+        let (app, temp_path) = setup();
 
         let result = app.run();
 
@@ -100,16 +108,38 @@ mod tests {
 
     #[test]
     fn test_app_run_fails_folder_exists() {
-        let temp_dir = tempdir().unwrap();
-        let temp_path = temp_dir.path().to_path_buf();
-        let args = AppArguments::new("test_project", "test details", Some(temp_path.to_str().unwrap().to_owned()));
-        let app = App::new(&args);
+        let (app, _) = setup();
 
         let result = app.run();
         let result2 = app.run();
 
         assert!(result.is_ok());
         assert!(result2.is_err());
-        assert_eq!(result2.unwrap_err().to_string(), "Error: Folder already exists. Please choose a different location.");
+        assert_eq!(
+            result2.unwrap_err().to_string(),
+            format!("Error: Folder already exists. Please choose a different location.")
+        );
+    }
+
+    #[test]
+    fn test_file_contents() {
+        let (app, temp_path) = setup();
+
+        app.run().unwrap();
+
+        let cargo_file = temp_path.join("test_project").join(CARGO_FILE_NAME);
+        let main_file = temp_path
+            .join("test_project")
+            .join(MAIN_FILE_PATH)
+            .join(MAIN_FILE_NAME);
+
+        let cargo_file_contents = fs::read_to_string(cargo_file).unwrap();
+        let main_file_contents = fs::read_to_string(main_file).unwrap();
+
+        assert_eq!(
+            cargo_file_contents,
+            format!(cargo_template_str!(), "test_project", "test details")
+        );
+        assert_eq!(main_file_contents, main_template_str!());
     }
 }
